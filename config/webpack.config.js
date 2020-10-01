69c9227a7d9621972bf1e96a0edac4e600990651
Raúl Caro Pastorino <dev@fryntiz.es>
2020-10-01 19:32:29 +0200

    Prepare webpack to resolve conflict with assets

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,10 @@
 const path = require('path');
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const IgnoreAssetsPlugin = require('ignore-assets-webpack-plugin');
+const toml = require('toml'); 
+const yaml = require('yamljs');
+const json5 = require('json5');
 
 module.exports = {
   entry: path.resolve(__dirname + './../src/index.js'),
@@ -8,7 +13,8 @@ module.exports = {
     filename: 'v-weather-chipiona.js'
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.js$/,
         use: 'babel-loader'
       },
@@ -31,9 +37,74 @@ module.exports = {
           'sass-loader',
         ]
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg|webp|wav|mp3|ogg)$/, 
+        use: "file-loader" 
+      },
+      {
+
+        test: /\.(csv|tsv)$/,
+
+        use: [
+
+          'csv-loader',
+
+        ],
+
+      },
+
+      {
+
+        test: /\.xml$/,
+
+        use: [
+
+          'xml-loader',
+
+        ],
+
+      },
+      {
+        test: /\.toml$/,
+        type: 'json',
+        parser: {
+          parse: toml.parse
+        }
+      },
+      {
+        test: /\.yaml$/,
+        type: 'json',
+        parser: {
+          parse: yaml.parse
+        }
+      },
+      {
+        test: /\.json5$/,
+        type: 'json',
+        parser: {
+          parse: json5.parse
+        }
+      }
     ]
   },
   plugins: [
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+        /*
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/assets', //Will resolve to RepoDir/src/assets 
+          to: 'assets' //Copies all files from above dest to dist/assets
+        }
+      ],
+      options: {
+        concurrency: 100,
+      },
+    }),
+        */
+
+    new IgnoreAssetsPlugin({
+      //ignore: 'asset.js'
+    })
   ]
 }
